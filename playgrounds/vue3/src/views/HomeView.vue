4e32018ff3b0c7f152fdeeb1a83535f43edb60d6
Daniel Hernández <danhernan23i@gmail.com>
2022-11-12 09:00:31 -0400

    feat(playground/vue3): Initial playground setup
    Will probably get replaced by docs in the future

---
+++ playgrounds/vue3/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { AirisStep, AirisStepper } from '@airis-ui/vue';
+import '@airis-ui/vue/styles';
+</script>
+
+<template>
+  <main>
+    <AirisStepper>
+      <AirisStep>
+        <div>Step 1</div>
+      </AirisStep>
+      <AirisStep>
+        <div>Step 2</div>
+      </AirisStep>
+      <AirisStep>
+        <div>Step 3</div>
+      </AirisStep>
+      <AirisStep>
+        <div>Step 4</div>
+      </AirisStep>
+    </AirisStepper>
+  </main>
+</template>
