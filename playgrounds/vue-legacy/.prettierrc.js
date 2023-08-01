ba94e51ef98f14ac3998dd27cfa288800ccc0a05
Daniel Hernández <danhernan23i@gmail.com>
2023-08-01 15:45:48 -0400

    feat: Add vue-legacy playground

---
+++ playgrounds/vue-legacy/.prettierrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ...require('@airis-ui/eslint-config/prettier'),
+};
---
+++ playgrounds/vue-legacy/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <AirisStepper keep-alive>
+    <AirisStep>A</AirisStep>
+    <AirisStep disabled>B</AirisStep>
+    <AirisStep>C</AirisStep>
+    <AirisStep disabled>D</AirisStep>
+    <AirisStep>F</AirisStep>
+  </AirisStepper>
+</template>
+
+<script>
+import '@airis-ui/vue-legacy/styles';
+import { AirisStep, AirisStepper } from '@airis-ui/vue-legacy';
+export default {
+  components: { AirisStep, AirisStepper },
+};
+</script>
