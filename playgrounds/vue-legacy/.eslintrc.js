fe4126542ee4c1eb22bcb87471a7cd97bbf560d7
Daniel Hernández <danhernan23i@gmail.com>
2023-08-01 15:56:45 -0400

    fix: eslint stuff

--- playgrounds/vue-legacy/.eslintrc.js
+++ playgrounds/vue-legacy/.eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     node: true,
   },
   extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
